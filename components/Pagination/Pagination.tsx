5f3823fcc0420fecddc14bedb6871598aee19f02
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-07-14 08:15:06 +0300

    fix pagination

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,3 +1,53 @@
+// import ReactPaginate from "react-paginate";
+// import css from "./Pagination.module.css";
+
+// interface PaginationProps {
+//   pageCount: number;
+//   currentPage: number;
+//   onPageChange: (page: number) => void;
+// }
+
+// export default function Pagination({
+//   pageCount,
+//   currentPage,
+//   onPageChange,
+// }: PaginationProps) {
+//   const cleanPageCount = Math.floor(Number(pageCount) || 0);
+//   const cleanCurrentPage = Math.floor(Number(currentPage) || 1);
+
+//   if (cleanPageCount <= 1) {
+//     return null;
+//   }
+
+//   const handlePageClick = (selectedItem: { selected: number }) => {
+//     onPageChange(selectedItem.selected + 1);
+//   };
+
+//   return (
+//     <nav className={css.paginationContainer}>
+//       <ReactPaginate
+//         pageCount={cleanPageCount}
+//         onPageChange={handlePageClick}
+//         forcePage={cleanCurrentPage - 1}
+//         previousLabel="< previous"
+//         nextLabel="next >"
+//         breakLabel="..."
+//         containerClassName={css.paginationList}
+//         pageClassName={css.pageItem}
+//         pageLinkClassName={css.pageLink}
+//         previousClassName={css.pageItem}
+//         previousLinkClassName={css.pageLink}
+//         nextClassName={css.pageItem}
+//         nextLinkClassName={css.pageLink}
+//         breakClassName={css.pageItem}
+//         breakLinkClassName={css.pageLink}
+//         activeClassName={css.active}
+//         disabledClassName={css.disabled}
+//       />
+//     </nav>
+//   );
+// }
+
 import ReactPaginate from "react-paginate";
 import css from "./Pagination.module.css";
 
@@ -24,7 +74,8 @@ export default function Pagination({
   };
 
   return (
-    <nav className={css.paginationContainer}>
+    // Тег nav залишаємо як обгортку, якщо він потрібен
+    <nav>
       <ReactPaginate
         pageCount={cleanPageCount}
         onPageChange={handlePageClick}
@@ -32,17 +83,19 @@ export default function Pagination({
         previousLabel="< previous"
         nextLabel="next >"
         breakLabel="..."
-        containerClassName={css.paginationList}
-        pageClassName={css.pageItem}
-        pageLinkClassName={css.pageLink}
-        previousClassName={css.pageItem}
-        previousLinkClassName={css.pageLink}
-        nextClassName={css.pageItem}
-        nextLinkClassName={css.pageLink}
-        breakClassName={css.pageItem}
-        breakLinkClassName={css.pageLink}
-        activeClassName={css.active}
-        disabledClassName={css.disabled}
+        // ЗВЕРНІТЬ УВАГУ СЮДИ: прив'язуємо класи точно до вашого Pagination.module.css
+        containerClassName={css.pagination} // відповідає класу .pagination
+        activeClassName={css.active} // відповідає класу .active
+        // Для елементів списку (li) та посилань (a) класи окремо передавати не потрібно,
+        // оскільки у вашому CSS написані каскадні правила: .pagination li та .pagination li a
+        pageClassName=""
+        pageLinkClassName=""
+        previousClassName=""
+        previousLinkClassName=""
+        nextClassName=""
+        nextLinkClassName=""
+        breakClassName=""
+        breakLinkClassName=""
       />
     </nav>
   );
